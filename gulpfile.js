64e3ed36508157c4856e8a8084abf37785ac8c24
Paul Julian <bpj101@dameagnesproductions.com>
2015-07-10 07:53:03 -0400

    Added testing for Mocha

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var karma = require('karma').server;
 
 gulp.task('serve', function () {
   browserSync.init({
@@ -9,6 +10,30 @@ gulp.task('serve', function () {
     port: 8080,
     server: {
       baseDir: ["app"],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch(['app/**/*.*'])
+    .on('change', browserSync.reload);
+});
+
+gulp.task('test-browser', function () {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true,
+    reporters: ['mocha']
+  });
+});
+
+gulp.task('serve-test', function () {
+  browserSync.init({
+    notify: false,
+    port: 8081,
+    server: {
+      baseDir: ["test", "app"],
       routes: {
         '/bower_components': 'bower_components'
       }
