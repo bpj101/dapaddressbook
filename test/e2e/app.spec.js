9acc8670ee3f115e5dfed8999a62421ea11fd371
Paul Julian <bpj101@dameagnesproductions.com>
2015-07-13 09:04:06 -0400

    Added protractor for testing

---
+++ test/e2e/app.spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('end to end address tests', function () {
+  it("should have contacts", function (done) {
+    browser.get('http://localhost:8080');
+    element.all(by.repeater('contact in contacts'))
+      .then(function (contacts) {
+        var first = contacts[0];
+        var text = first.getText();
+        expect(text).toEqual('Dennis D');
+        done();
+      });
+  });
+});
